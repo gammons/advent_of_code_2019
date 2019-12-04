4ac5146916654f445573d780672d456f4db96078
Grant Ammons <gammons@gmail.com>
2019-12-04 06:53:29 -0500

    cleanup

--- day_3/wires1.rb
+++ day_3/wires1.rb
@@ -1,75 +1,70 @@
 require "byebug"
 
-lines = File.read("input.txt").split("\n")
+class WireIntersector
+  def initialize
+    @wire_grid = Array.new(10000) { Array.new(10000) }
+    @intersections = []
+  end
 
+  def nearest_intersection(wire_locations1, wire_locations2)
+    populate_grid(wire_locations1)
+    populate_grid(wire_locations2)
+    manhatten
+  end
+
+  private
+
+  def populate_grid(wire)
+    x = 0
+    y = 0
+
+    wire.each do |instruction|
+      direction = instruction[0]
+      moves = instruction[1..(instruction.length)].to_i
+
+      case direction
+      when "R"
+        moves.times do
+          x += 1
+          populate_grid_point(x,y)
+        end
+      when "L"
+        moves.times do
+          x -= 1
+          populate_grid_point(x,y)
+        end
+      when "U"
+        moves.times do
+          y += 1
+          populate_grid_point(x,y)
+        end
+      when "D"
+        moves.times do
+          y -= 1
+          populate_grid_point(x,y)
+        end
+      end
+    end
+  end
+
+  def manhatten
+    min_location[0].abs + min_location[1].abs
+  end
+
+  def min_location
+    @min_location ||= @intersections.min {|l1,l2| (l1[0].abs + l1[1].abs) <=> (l2[0].abs + l2[1].abs) }
+  end
+
+  def populate_grid_point(x,y)
+    if @wire_grid[x][y] == 1
+      @intersections.push([x,y])
+    end
+    @wire_grid[x][y] = 1
+  end
+end
+
+lines = File.read("input.txt").split("\n")
 wire1 = lines[0].split(",")
 wire2 = lines[1].split(",")
 
-wire_grid1 = Array.new(10000) { Array.new(10000) }
-wire_grid2 = Array.new(10000) { Array.new(10000) }
-
-class Location
-  attr_accessor :x,:y
-
-  def initialize(x,y)
-    @x = x
-    @y = y
-  end
-
-  def to_s
-    "(#{@x},#{@y})"
-  end
-end
-
-def get_wire_locations(wire, wire_grid)
-  current = Location.new(0,0)
-
-  wire_locations = []
-  wire.each do |instruction|
-    chars = instruction.chars
-    direction = chars.shift
-    moves = chars.join.to_i
-
-    case direction
-    when "R"
-      (current.x..(current.x + moves)).each do |x|
-        wire_locations.push(Location.new(x, current.y))
-        wire_grid[x][current.y] = true
-      end
-      current = Location.new(current.x + moves,current.y)
-    when "L"
-      (current.x.downto(current.x - moves)).each do |x|
-        wire_locations.push(Location.new(x, current.y))
-        wire_grid[x][current.y] = true
-      end
-      current = Location.new(current.x - moves,current.y)
-    when "U"
-      (current.y..(current.y + moves)).each do |y|
-        wire_locations.push(Location.new(current.x, y))
-        wire_grid[current.x][y] = true
-      end
-      current = Location.new(current.x,current.y + moves)
-    when "D"
-      (current.y.downto(current.y - moves)).each do |y|
-        wire_locations.push(Location.new(current.x, y))
-        wire_grid[current.x][y] = true
-      end
-      current = Location.new(current.x,current.y - moves)
-    end
-  end
-  wire_locations
-end
-
-wire1_locations = get_wire_locations(wire1, wire_grid1)
-wire2_locations = get_wire_locations(wire2, wire_grid2)
-
-intersections = []
-wire2_locations.each do |loc|
-  if wire_grid1[loc.x][loc.y] == true
-    intersections.push(loc) unless loc.x == 0 && loc.y == 0
-  end
-end
-
-min_location = intersections.min {|l1,l2| (l1.x.abs + l1.y.abs) <=> (l2.x.abs + l2.y.abs) }
-
-puts min_location.x.abs + min_location.y.abs
+puts WireIntersector.new.nearest_intersection(wire1, wire2)
