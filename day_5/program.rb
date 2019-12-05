c0d5e2647c06276fdc807f98f56d542172fe84d5
Grant Ammons <gammons@gmail.com>
2019-12-05 10:06:07 -0500

    refactor

--- day_5/program.rb
+++ day_5/program.rb
@@ -14,11 +14,10 @@ module PARAMETER_MODES
 end
 
 class Instruction
-  def initialize(opcode, ip, params, data)
+  def initialize(ip, data)
     @ip = ip
-    @opcode = opcode
-    @params = params
     @data = data
+    @opcode = @data[@ip]
   end
 
   def process; end
@@ -26,9 +25,9 @@ class Instruction
   def value_for(param_num)
     case parameter_mode_for(param_num)
     when PARAMETER_MODES::IMMEDIATE
-      @params[param_num].to_i
+      params[param_num].to_i
     else
-      @data[@params[param_num].to_i].to_i
+      @data[params[param_num].to_i].to_i
     end
   end
 
@@ -38,19 +37,35 @@ class Instruction
   end
 
   def next_ip_position
-    @ip + @params.length + 1
+    @ip + params.length + 1
+  end
+
+  def number_of_params
+    0
+  end
+
+  def params
+    @data[(@ip + 1)..(@ip + number_of_params)]
   end
 end
 
 class AddInstruction < Instruction
   def process
-    @data[@params[2].to_i] = (value_for(0) + value_for(1)).to_s
+    @data[params[2].to_i] = (value_for(0) + value_for(1)).to_s
+  end
+
+  def number_of_params
+    3
   end
 end
 
 class MultiplyInstruction < Instruction
   def process
-    @data[@params[2].to_i] = (value_for(0) * value_for(1)).to_s
+    @data[params[2].to_i] = (value_for(0) * value_for(1)).to_s
+  end
+
+  def number_of_params
+    3
   end
 end
 
@@ -58,13 +73,27 @@ class InputInstruction < Instruction
   def process
     puts "Enter input: "
     input = gets
-    @data[@params[0].to_i] = input
+    @data[params[0].to_i] = input
+  end
+
+  def number_of_params
+    1
   end
 end
 
 class OutputInstruction < Instruction
   def process
     puts "Value for instruction: #{value_for(0)}"
+  end
+
+  def number_of_params
+    1
+  end
+end
+
+class EndInstruction < Instruction
+  def process
+    exit
   end
 end
 
@@ -91,32 +120,21 @@ class Computer
       opcode = (raw_opcode.chars[raw_opcode.chars.length - 2..raw_opcode.chars.length].join).to_i.to_s
     end
 
-    params = []
-    params = @data[(@ip + 1)..(@ip + param_count)] if opcode != INSTRUCTIONS::O_END
-
     debug "    raw_opcode = '#{raw_opcode}'"
     debug "    opcode = '#{opcode}'"
-    debug "    params = #{params}"
     case opcode
     when INSTRUCTIONS::ADD
-      AddInstruction.new(@data[@ip], @ip, params, @data)
+      AddInstruction.new(@ip, @data)
     when INSTRUCTIONS::MULTIPLY
-      MultiplyInstruction.new(@data[@ip], @ip, params, @data)
+      MultiplyInstruction.new(@ip, @data)
     when INSTRUCTIONS::INPUT
-      InputInstruction.new(@data[@ip], @ip, params, @data)
+      InputInstruction.new(@ip, @data)
     when INSTRUCTIONS::OUTPUT
-      OutputInstruction.new(@data[@ip], @ip, params, @data)
+      OutputInstruction.new(@ip, @data)
     when INSTRUCTIONS::O_END
+      EndInstruction.new(@ip, @data)
       exit
     end
-  end
-
-  def number_of_params(raw_opcode)
-    raw_opcode.reverse[0]
-    return 3 if raw_opcode.reverse[0] == "1"
-    return 3 if raw_opcode.reverse[0] == "2"
-    return 1 if raw_opcode.reverse[0] == "3"
-    return 1 if raw_opcode.reverse[0] == "4"
   end
 
   def debug(msg)
