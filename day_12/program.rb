239f54e12fdd937397a0a38b9e5a52f500a7eace
Grant Ammons <gammons@gmail.com>
2019-12-13 07:26:42 -0500

    WIP

--- day_12/program.rb
+++ day_12/program.rb
@@ -6,6 +6,9 @@ class Moon
 
   def initialize(name, x,y,z)
     @name = name
+    @initial_x = x
+    @initial_y = y
+    @initial_z = z
     @x = x
     @y = y
     @z = z
@@ -22,6 +25,17 @@ class Moon
     @vx.abs + @vy.abs + @vz.abs
   end
 
+  def at_initial_position?(coord)
+    case coord
+    when :x
+      return @x == @initial_x
+    when :y
+      return @y == @initial_y
+    when :z
+      return @z == @initial_z
+    end
+  end
+
   def to_s
     "pos=<x=#{@x}, y=#{@y}, z=#{@z}, vel=<x=#{@vx}, y=#{@vy}, z=#{@vz}>"
   end
@@ -30,22 +44,45 @@ end
 class System
   def initialize
     @moons = []
-    @moons.push Moon.new("Io",-8, -18, 6)
-    @moons.push Moon.new("Europa",-11, -14, 4)
-    @moons.push Moon.new("Ganymede", 8, -3, -10)
-    @moons.push Moon.new("Callisto",-2, -16, 1)
+    # @moons.push Moon.new("Io",-8, -18, 6)
+    # @moons.push Moon.new("Europa",-11, -14, 4)
+    # @moons.push Moon.new("Ganymede", 8, -3, -10)
+    # @moons.push Moon.new("Callisto",-2, -16, 1)
+
+    @moons.push Moon.new("Io",-8, -10, 0)
+    @moons.push Moon.new("Europa",5, 5, 10)
+    @moons.push Moon.new("Ganymede", 2, -7, 3)
+    @moons.push Moon.new("Callisto",9,-8,-3)
   end
 
   def run
-    1000.times do |n|
-      apply_gravity
-      apply_velocity
-    end
-    puts "Total energy: #{total_energy}"
-  end
+    lcms = []
+    @moons.each do |moon|
+      x_count, y_count, z_count = nil, nil, nil
+      count = 0
 
-  def total_energy
-    @moons.inject(0) {|sum, m| sum + (m.potential * m.kinetic) }
+      loop do
+        count += 1
+        apply_gravity
+        apply_velocity
+
+        x_count = count if moon.at_initial_position?(:x) && moon.vx == 0 && count != 0 && x_count.nil?
+        y_count = count if moon.at_initial_position?(:y) && moon.vy == 0 && count != 0 && y_count.nil?
+        z_count = count if moon.at_initial_position?(:z) && moon.vz == 0 && count != 0 && z_count.nil?
+
+        break if x_count && y_count && z_count
+      end
+
+      lcms.push [x_count,y_count,z_count].reduce(:lcm)
+
+      puts "\nfor moon #{moon.name}"
+      puts "x_count = #{x_count}"
+      puts "y_count = #{y_count}"
+      puts "z_count = #{z_count}"
+      puts "lcm: #{lcms.last}"
+    end
+
+    puts "Total total: #{lcms.reduce(:lcm)}"
   end
 
   def apply_gravity
@@ -73,13 +110,6 @@ class System
       moon.y += moon.vy
       moon.z += moon.vz
     end
-  end
-
-  private
-
-  def debug(n)
-    puts "After #{n} steps:"
-    @moons.each {|moon| p moon }
   end
 end
 
